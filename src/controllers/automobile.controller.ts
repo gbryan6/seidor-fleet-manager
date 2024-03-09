16fc5c580cf84c2ec9c94a03a78b308f9a3b77f8
Gabriel <gbryan6@hotmail.com>
2024-03-08 21:27:11 -0300

    feat: Cria rota para criar automovel

--- src/controllers/automobile.controller.ts
+++ src/controllers/automobile.controller.ts
@@ -3,11 +3,29 @@ import { prisma } from "../server";
 
 const createAutomobile = async (req: Request, res: Response) => {
   try {
-    res.status(200).json({ message: 'ok'});
-  } catch (e) {
-    res.status(500).json({ error: e });
+    const { plate, color, brand } = req.body;
+
+    const existingAutomobile = await prisma.automobile.findUnique({
+      where: { plate },
+    });
+
+    if (existingAutomobile) {
+      return res.status(400).json({ error: "Plate already registered" });
+    }
+
+    const automobile = await prisma.automobile.create({
+      data: {
+        plate,
+        color,
+        brand,
+      },
+    });
+    res.status(201).json(automobile);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 };
+
 
 export default {
   createAutomobile
