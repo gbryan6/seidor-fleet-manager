992ef6e58004d5dd9dbe681ac0cdf6a887d2f6de
Gabriel <gbryan6@hotmail.com>
2024-03-08 23:37:21 -0300

    feat: Cria rota para criar motorista

--- src/controllers/driver.controller.ts
+++ src/controllers/driver.controller.ts
@@ -3,9 +3,17 @@ import { prisma } from "../server";
 
 const createDriver = async (req: Request, res: Response) => {
   try {
-    res.status(200).json({ message: 'ok'});
-  } catch (e) {
-    res.status(500).json({ error: e });
+    const { name } = req.body;
+
+    const driver = await prisma.driver.create({
+      data: {
+        name,
+      },
+    });
+
+    res.status(201).json(driver);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
